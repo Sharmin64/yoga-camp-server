d2d0adbd0dc5c21bd9692b0154cd5f355102c50c
Sharmin64 <lizaeatsbanana@gmail.com>
2023-06-12 20:00:04 +0600

    something is changed in index file

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ async function run() {
     const campCollection = client.db("summerDb").collection("classes");
 
     //?users related apis
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = {email: user.email};
