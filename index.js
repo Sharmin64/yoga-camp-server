7d0d1846bad13b44524e562c6d924050c2d57cd0
Sharmin64 <lizaeatsbanana@gmail.com>
2023-07-04 16:56:02 +0600

    data show on clint side done

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ const port = process.env.PORT || 5003;
 //? middleware
 app.use(cors());
 app.use(express.json());
-//const corsConfig = {
-//  origin: "",
-//  credentials: true,
-//  methods: ["GET", "POST", "PUT", "DELETE"],
-//};
-//app.use(cors(corsConfig));
-//app.options("", cors(corsConfig));
-//app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xol1uc7.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect(); //?vercel deploy korer somoy ey line comment kore rekhe terpor deploy  korte hobe
     const usersCollection = client.db("summerDb").collection("users");
     const instructorsCollection = client
       .db("summerDb")
@@ -40,13 +32,11 @@ async function run() {
 
     const campCollection = client.db("summerDb").collection("classes");
 
-    //?jwt post related
-    app.post("/jwt", (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "2h",
-      });
-      res.send({token});
+    app.post("/classes", async (req, res) => {
+      const body = req.body;
+      console.log("body:", body);
+      const result = await campCollection.insertOne(body);
+      res.send(result);
     });
 
     //?users related apis
@@ -60,7 +50,7 @@ async function run() {
       const result = await instructorsCollection.find().toArray();
       res.send(result);
     });
-    app.post("/users", async (req, res) => {
+    app.post("/email/users", async (req, res) => {
       const user = req.body;
       const query = {email: user.email};
       const existingUser = await usersCollection.findOne(query);
@@ -100,6 +90,8 @@ async function run() {
     });
 
     app.get("/classes", async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
       const result = await campCollection.find().toArray();
       res.send(result);
     });
